7ab78b74389b51be2347e118fc1c0f31c19d644a
Yuriy Titov <yurtitov@gmail.com>
2020-09-09 11:34:46 +0200

    yandex sdk has added

--- app/src/main/java/ru/okcode/currencyconverter/App.kt
+++ app/src/main/java/ru/okcode/currencyconverter/App.kt
@@ -1,7 +1,19 @@
 package ru.okcode.currencyconverter
 
 import android.app.Application
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Creating an extended library configuration.
+        val config: YandexMetricaConfig = YandexMetricaConfig.newConfigBuilder(BuildConfig.YA_KEY).build()
+        // Initializing the AppMetrica SDK.
+        YandexMetrica.activate(applicationContext, config)
+        // Automatic tracking of user activity.
+        YandexMetrica.enableActivityAutoTracking(this)
+    }
+}
